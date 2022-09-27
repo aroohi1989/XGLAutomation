446e1bd62b843887d4b13cd2fb9cbf1829ca4a89
Afshan <afshan.shoeb09@gmail.com>
2022-09-27 22:51:02 +0530

    Initial commit

---
+++ src/Day1.java
@@ -0,0 +1,12 @@
+public class Day1
+{
+    public static void main(String []args)
+        {
+            int num1=8;
+            int num2=9;
+            int num3=8;
+            int sum= num1+num2+num3;
+            System.out.println(sum);
+
+        }
+}
---
+++ src/MyFirstAssignment.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MyFirstAssignment
+{
+    public static void main(String[] args)
+    {
+        byte math;byte science; byte geography; byte english; byte hindi;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Please enter marks in Math");
+        math=sc.nextByte();
+        System.out.println("Please enter marks in Science");
+        science=sc.nextByte();
+        System.out.println("Please enter marks in Geography");
+        geography =sc.nextByte();
+        System.out.println("Please enter marks in english");
+        english =sc.nextByte();
+        System.out.println("Please enter marks in hindi");
+        hindi =sc.nextByte();
+        int total;
+        total= math+science+geography+english+hindi;
+        float percentage= (total*100)*500;
+        System.out.println("Total is "+total);
+        System.out.println("The percentage is "+percentage);
+
+    }
+}
